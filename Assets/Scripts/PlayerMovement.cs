fb77f314ab0b20099c08fe973de30085fdd7153a
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-09-25 15:57:20 -0500

    Player Movement
    Can move all but vertically

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody Rigidbody;
+    [SerializeField]
+    private float speed = 20;
+
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        Rigidbody.AddForce(movement );
+    }
+}
